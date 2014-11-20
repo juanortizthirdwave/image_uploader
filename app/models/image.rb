34543d1d92b38ae9bfbd2088c7679ea01e09c630
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-20 14:47:47 -0600

    removed cattr_accessor and updated fields method

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,12 @@
 class Image < ActiveRecord::Base
-  cattr_accessor(:field_ids) { Array.new }
   belongs_to :gallery
-  has_paper_trail
   serialize :field_ids, Array
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   do_not_validate_attachment_file_type :picture 
+  has_paper_trail
 
   def fields
-    return self.field_ids if self.field_ids.empty?
-    self.field_ids.map { |e| Field.find e }
+    return field_ids if field_ids.empty?
+    field_ids.map { |e| Field.find e }
   end
 end
