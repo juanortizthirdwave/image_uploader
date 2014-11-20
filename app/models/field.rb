28847093a223fe8bc1fd37c2269b8a9a99ab9de8
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-20 10:39:53 -0600

    commented out active record association, callbacks and paper_trail gem from field model

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,14 +1,14 @@
 class Field < ActiveRecord::Base
-  belongs_to :image
-  has_paper_trail
+  # belongs_to :image
+  # has_paper_trail
 
   # after_update :update_image
-  after_save :update_image
+  # after_save :update_image
 
 
   private
 
-  def update_image
-    self.image.touch_with_version
-  end
+  # def update_image
+  #   self.image.touch_with_version
+  # end
 end
