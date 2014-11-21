457c25d936c25deb37661d1650ea1a12b6842bdc
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-21 16:32:03 -0600

    preparing Image for custom_fields attribute

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
-
-  has_many :fields
+  cattr_accessible :custom_fields { Hash.new }
   belongs_to :gallery
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  serialize :custom_fields, Hash
 
   # this is for the demos app, check if you should uncomment this line or add a validation
   do_not_validate_attachment_file_type :picture 
