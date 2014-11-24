4c7b28f7ac79a97cfe0c4c1dfcda926c3c31f17e
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-24 11:46:20 -0600

    skipping versioning attributes that won't change in the Image field. Better organizing the macros

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,18 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   serialize :fields, Hash
 
+
+  # ********* file attachment ********
+
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   # this is for the demos app, check if you should uncomment this line or add a validation
   do_not_validate_attachment_file_type :picture 
 
-  has_paper_trail #:skip => [:gallery_id, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at]
+
+  # ********* versioning the object *********
+
+  has_paper_trail :skip => [:gallery_id, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at]
 
   def fields
     read_attribute(:fields) || {}
