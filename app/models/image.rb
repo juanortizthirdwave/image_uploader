122686ed0b32e3afb419be25568af8fd452d7e84
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-11-24 11:37:21 -0600

    added custom reader method for fields

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  serialize :custom_fields, Hash
+  serialize :fields, Hash
 
   # this is for the demos app, check if you should uncomment this line or add a validation
   do_not_validate_attachment_file_type :picture 
 
-  has_paper_trail :skip => [:gallery_id, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at]
+  has_paper_trail #:skip => [:gallery_id, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at]
+
+  def fields
+    read_attribute(:fields) || {}
+  end
 end
